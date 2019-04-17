90d39defc7047841db56917bcfca55b19f51236d
JinYang0323 <jinyang0323@gmail.com>
2019-04-17 13:40:06 -0700

    altered orderstep 1 and summary

--- src/Order/OrderStep1.js
+++ src/Order/OrderStep1.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Redirect } from "react-router-dom";
-import ExteriorOptions from "./orderStep1/ExteriorOptions";
-import styles from "./OrderStep1.module.css";
+import { Redirect } from 'react-router-dom';
+import ExteriorOptions from './orderStep1/ExteriorOptions';
+import styles from './OrderStep1.module.css';
 
 class OrderStep1 extends Component {
-    constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
       submittedSuccessfully: false
-    }
+    };
   }
 
   handleSubmit() {
@@ -20,68 +20,69 @@ class OrderStep1 extends Component {
   }
 
   render() {
-    const {
-      selectedProductId,
-        categories
-    } = this.props;
+    const { selectedProductId, categories } = this.props;
     let selectedProductCategoryId = null;
     const selectedProduct = this.props.products[selectedProductId];
 
     let opt;
     if (selectedProduct) {
-        selectedProductCategoryId = selectedProduct.categoryId;
-        const category = categories[selectedProductCategoryId];
-        opt = (
-            <div className={styles.container}>
-                
-                <div>
-                    {/*{<PowerOptions className={styles["panel"]}*/}
-                                      {/*setProductOption = {this.props.setProductOption}*/}
-                                      {/*selectedProduct={selectedProductCategoryId}*/}
-                                        {/*selectedOptions={this.props.selectedOptions}*/}
-                                      {/*options={this.props.options}/>}*/}
-                    {/*{<InteriorOptions*/}
-                        {/*setProductOption = {this.props.setProductOption}*/}
-                        {/*selectedProduct={selectedProductCategoryId}*/}
-                        {/*selectedOptions={this.props.selectedOptions}*/}
-                        {/*options={this.props.options}/>}*/}
-                    {<ExteriorOptions
-                        setProductOption = {this.props.setProductOption}
-                        selectedProduct={selectedProductCategoryId}
-                        selectedOptions={this.props.selectedOptions}
-                        options={this.props.options}/>}
-                    {/*{<PremiumFeatures*/}
-                        {/*setProductOption = {this.props.setProductOption}*/}
-                        {/*selectedProduct={selectedProductCategoryId}*/}
-                        {/*selectedOptions={this.props.selectedOptions}*/}
-                        {/*options={this.props.options}/>}*/}
-                </div>
-                <div className={styles["bottom-panel"]}>
-                    <img className={styles.image} alt="CarImage" src={category.img.lg} />
-                </div>
-            </div>
-
-        );
+      selectedProductCategoryId = selectedProduct.categoryId;
+      const category = categories[selectedProductCategoryId];
+      opt = (
+        <div className={styles.container}>
+          <div>
+            {/*{<PowerOptions className={styles["panel"]}*/}
+            {/*setProductOption = {this.props.setProductOption}*/}
+            {/*selectedProduct={selectedProductCategoryId}*/}
+            {/*selectedOptions={this.props.selectedOptions}*/}
+            {/*options={this.props.options}/>}*/}
+            {/*{<InteriorOptions*/}
+            {/*setProductOption = {this.props.setProductOption}*/}
+            {/*selectedProduct={selectedProductCategoryId}*/}
+            {/*selectedOptions={this.props.selectedOptions}*/}
+            {/*options={this.props.options}/>}*/}
+            {
+              <ExteriorOptions
+                setProductOption={this.props.setProductOption}
+                selectedProduct={selectedProductCategoryId}
+                selectedOptions={this.props.selectedOptions}
+                options={this.props.options}
+              />
+            }
+            {/*{<PremiumFeatures*/}
+            {/*setProductOption = {this.props.setProductOption}*/}
+            {/*selectedProduct={selectedProductCategoryId}*/}
+            {/*selectedOptions={this.props.selectedOptions}*/}
+            {/*options={this.props.options}/>}*/}
+          </div>
+          <div className={styles['bottom-panel']}>
+            <img
+              className={styles.image}
+              alt='CarImage'
+              src={category.img.lg}
+            />
+          </div>
+        </div>
+      );
     }
-    return this.state.submittedSuccessfully
-      ? (<Redirect to="/order/2" />)
-      : (
+    return this.state.submittedSuccessfully ? (
+      <Redirect to='/order/2' />
+    ) : (
       <form onSubmit={this.handleSubmit.bind(this)}>
-          {opt}
+        {opt}
         <fieldset className={styles.button}>
-          <input type="submit" value="Go to Step 2" />
+          <input className={styles.submit} type='submit' value='Go to Step 2' />
         </fieldset>
       </form>
-
-    )
+    );
   }
 }
 
 OrderStep1.propTypes = {
-    options: PropTypes.object.isRequired,
-    selectedProductId: PropTypes.string,
-    selectedOptions: PropTypes.object.isRequired,
-    setProductOption: PropTypes.func.isRequired,
+  options: PropTypes.object.isRequired,
+  selectedProductId: PropTypes.string,
+  selectedOptions: PropTypes.object.isRequired,
+  setProductOption: PropTypes.func.isRequired
 };
 
 export default OrderStep1;
